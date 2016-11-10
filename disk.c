6a7748121823c96f1725776697fd8c4b5bc90c21
Златан Димитров <zlatan.dimitrov@gmail.com>
2016-11-10 22:34:36 +0200

    Update disk.c

--- disk.c
+++ disk.c
@@ -5,10 +5,13 @@ int main(void)
 {
 
 	double Qx0=20., Qy0=3., Qz0=3.;
+	Qx0=Qy0=Qz0=3.;// xxxx
 	int Nx=30, Ny=6, Nz=6;
+	Nx=10, Ny=10, Nz=10;//****
 	int NDimx=2*Nx+2, NDimy=2*Ny+2, NDimz=2*Nz+2;
 	double vx[NDimx][NDimy][NDimz], vy[NDimx][NDimy][NDimz], vz[NDimx][NDimy][NDimz],
 	bx[NDimx][NDimy][NDimz], by[NDimx][NDimy][NDimz], bz[NDimx][NDimy][NDimz],
+	DV[NDimx][NDimy][NDimz],
 	bbyx[NDimx][NDimy][NDimz], bbyy[NDimx][NDimy][NDimz], bbyz[NDimx][NDimy][NDimz],
 	bbxx[NDimx][NDimy][NDimz], bbxy[NDimx][NDimy][NDimz], bbxz[NDimx][NDimy][NDimz],
 	bbzx[NDimx][NDimy][NDimz], bbzy[NDimx][NDimy][NDimz], bbzz[NDimx][NDimy][NDimz],
@@ -25,16 +28,17 @@ int main(void)
 	rnbx[NDimx][NDimy][NDimz], rnby[NDimx][NDimy][NDimz], rnbz[NDimx][NDimy][NDimz];
 	int Lx=2*Nx, Ly= 2*Ny, Lz=2*Nz;
 	double DQx=2*Qx0/(2*Nx), DQy=2*Qy0/(2*Ny), DQz=2*Qz0/(2*Nz), rnx, rny, rnz, vn, bn;
-  double Q=0.0;
+	double DVQ=DQx*DQy*DQz/((2*M_PI)*(2*M_PI)*(2*M_PI));
+    double Q=0.0;
 
 	for (int ix=0;ix<=Lx; ix++)
-	{//Qx=-Qx0+ix*DQx;
+	{double Qx=-Qx0+ix*DQx;
 		for (int iy=0;iy<=Ly; iy++)
-		{//Qy=-Qy0+iy*DQy;
+		{double Qy=-Qy0+iy*DQy;
 			for (int iz=0;iz<=Lz; iz++)
-			{//Qz=-Qz0+iz*DQz;
+			{double Qz=-Qz0+iz*DQz;
 		bbxx[ix][iy][iz]= bbxy[ix][iy][iz]= bbxz[ix][iy][iz]=
-	  bbyx[ix][iy][iz]= bbyy[ix][iy][iz]= bbyz[ix][iy][iz]=
+		bbyx[ix][iy][iz]= bbyy[ix][iy][iz]= bbyz[ix][iy][iz]=
 		bbzx[ix][iy][iz]= bbzy[ix][iy][iz]= bbzz[ix][iy][iz]=
 		vvxx[ix][iy][iz]= vvxy[ix][iy][iz]= vvxz[ix][iy][iz]=
 		vvyx[ix][iy][iz]= vvyy[ix][iy][iz]= vvyz[ix][iy][iz]=
@@ -45,6 +49,8 @@ int main(void)
 		vbxx[ix][iy][iz]= vbxy[ix][iy][iz]= vbxz[ix][iy][iz]=
 		vbyx[ix][iy][iz]= vbyy[ix][iy][iz]= vbyz[ix][iy][iz]=
 		vbzx[ix][iy][iz]= vbzy[ix][iy][iz]= vbzz[ix][iy][iz]=0.0;
+		vx[ix][iy][iz]=exp(-(Qx*Qx+Qy*Qy+Qz*Qz))
+		DV[ix][iy][iz]=DVQ;
 			// summation cycles for nonlinear terms
 				for (int jx=0;jx<=Lx; jx++)
 				{int kx=ix-jx+Nx; if(kx<0) continue; if(kx>Lx) continue;
@@ -54,21 +60,21 @@ int main(void)
 						{// razgele, naj setne zapoichvame rabota
 						 int kz=iz-jz+Nz; if(kz<0) continue; if(kz>Lz) continue;
 						 //bb
-					    bbxx[ix][iy][iz]+=bx[jx][jy][jz]*bx[kx][ky][kz]; bbxy[ix][iy][iz]+=bx[jx][jy][jz]*by[kx][ky][kz]; bbxz[ix][iy][iz]+=bx[jx][jy][jz]*bz[kx][ky][kz];
-							bbyx[ix][iy][iz]+=by[jx][jy][jz]*bx[kx][ky][kz]; bbyy[ix][iy][iz]+=by[jx][jy][jz]*by[kx][ky][kz]; bbyz[ix][iy][iz]+=by[jx][jy][jz]*bz[kx][ky][kz];
-							bbzx[ix][iy][iz]+=bz[jx][jy][jz]*bx[kx][ky][kz]; bbzy[ix][iy][iz]+=by[jx][jy][jz]*by[kx][ky][kz]; bbzz[ix][iy][iz]+=bz[jx][jy][jz]*bz[kx][ky][kz];
+							bbxx[ix][iy][iz]+=bx[jx][jy][jz]*bx[kx][ky][kz]*DV[jx][jy][jz]; bbxy[ix][iy][iz]+=bx[jx][jy][jz]*by[kx][ky][kz]*DV[jx][jy][jz]; bbxz[ix][iy][iz]+=bx[jx][jy][jz]*bz[kx][ky][kz]*DV[jx][jy][jz];
+							bbyx[ix][iy][iz]+=by[jx][jy][jz]*bx[kx][ky][kz]*DV[jx][jy][jz]; bbyy[ix][iy][iz]+=by[jx][jy][jz]*by[kx][ky][kz]*DV[jx][jy][jz]; bbyz[ix][iy][iz]+=by[jx][jy][jz]*bz[kx][ky][kz]*DV[jx][jy][jz];
+							bbzx[ix][iy][iz]+=bz[jx][jy][jz]*bx[kx][ky][kz]*DV[jx][jy][jz]; bbzy[ix][iy][iz]+=by[jx][jy][jz]*by[kx][ky][kz]*DV[jx][jy][jz]; bbzz[ix][iy][iz]+=bz[jx][jy][jz]*bz[kx][ky][kz]*DV[jx][jy][jz];
 						 //vv
-							vvxx[ix][iy][iz]+=vx[jx][jy][jz]*vx[kx][ky][kz]; vvxy[ix][iy][iz]+=vx[jx][jy][jz]*vy[kx][ky][kz]; vvxz[ix][iy][iz]+=vx[jx][jy][jz]*vz[kx][ky][kz];
-							vvyx[ix][iy][iz]+=vy[jx][jy][jz]*vx[kx][ky][kz]; vvyy[ix][iy][iz]+=vy[jx][jy][jz]*vy[kx][ky][kz]; vvyz[ix][iy][iz]+=vy[jx][jy][jz]*vz[kx][ky][kz];
-							vvzx[ix][iy][iz]+=vz[jx][jy][jz]*vx[kx][ky][kz]; vvzy[ix][iy][iz]+=vy[jx][jy][jz]*vy[kx][ky][kz]; vvzz[ix][iy][iz]+=vz[jx][jy][jz]*vz[kx][ky][kz];
+							vvxx[ix][iy][iz]+=vx[jx][jy][jz]*vx[kx][ky][kz]*DV[jx][jy][jz]; vvxy[ix][iy][iz]+=vx[jx][jy][jz]*vy[kx][ky][kz]*DV[jx][jy][jz]; vvxz[ix][iy][iz]+=vx[jx][jy][jz]*vz[kx][ky][kz]*DV[jx][jy][jz];
+							vvyx[ix][iy][iz]+=vy[jx][jy][jz]*vx[kx][ky][kz]*DV[jx][jy][jz]; vvyy[ix][iy][iz]+=vy[jx][jy][jz]*vy[kx][ky][kz]*DV[jx][jy][jz]; vvyz[ix][iy][iz]+=vy[jx][jy][jz]*vz[kx][ky][kz]*DV[jx][jy][jz];
+							vvzx[ix][iy][iz]+=vz[jx][jy][jz]*vx[kx][ky][kz]*DV[jx][jy][jz]; vvzy[ix][iy][iz]+=vz[jx][jy][jz]*vy[kx][ky][kz]*DV[jx][jy][jz]; vvzz[ix][iy][iz]+=vz[jx][jy][jz]*vz[kx][ky][kz]*DV[jx][jy][jz];
 						 //bv
-							bvxx[ix][iy][iz]+=bx[jx][jy][jz]*vx[kx][ky][kz]; bvxy[ix][iy][iz]+=bx[jx][jy][jz]*vy[kx][ky][kz]; bvxz[ix][iy][iz]+=bx[jx][jy][jz]*vz[kx][ky][kz]; // <<<<<<<<<<< DA SE PROVERI FORMULATA bvxz[ix][iy][iz]+=bx[jx][jy][jz]*vv[kx][ky][kz];
-							bvyx[ix][iy][iz]+=by[jx][jy][jz]*vx[kx][ky][kz]; bbyy[ix][iy][iz]+=by[jx][jy][jz]*vy[kx][ky][kz]; bbyz[ix][iy][iz]+=by[jx][jy][jz]*vz[kx][ky][kz];
-							bvzx[ix][iy][iz]+=bz[jx][jy][jz]*vx[kx][ky][kz]; bbzy[ix][iy][iz]+=by[jx][jy][jz]*vy[kx][ky][kz]; bbzz[ix][iy][iz]+=bz[jx][jy][jz]*vz[kx][ky][kz];
+							bvxx[ix][iy][iz]+=bx[jx][jy][jz]*vx[kx][ky][kz]*DV[jx][jy][jz]; bvxy[ix][iy][iz]+=bx[jx][jy][jz]*vy[kx][ky][kz]*DV[jx][jy][jz]; bvxz[ix][iy][iz]+=bx[jx][jy][jz]*vz[kx][ky][kz]*DV[jx][jy][jz];
+							bvyx[ix][iy][iz]+=by[jx][jy][jz]*vx[kx][ky][kz]*DV[jx][jy][jz]; bvyy[ix][iy][iz]+=by[jx][jy][jz]*vy[kx][ky][kz]*DV[jx][jy][jz]; bvyz[ix][iy][iz]+=by[jx][jy][jz]*vz[kx][ky][kz]*DV[jx][jy][jz];
+							bvzx[ix][iy][iz]+=bz[jx][jy][jz]*vx[kx][ky][kz]*DV[jx][jy][jz]; bvzy[ix][iy][iz]+=bz[jx][jy][jz]*vy[kx][ky][kz]*DV[jx][jy][jz]; bvzz[ix][iy][iz]+=bz[jx][jy][jz]*vz[kx][ky][kz]*DV[jx][jy][jz];
 						 //vb
-						  vvxx[ix][iy][iz]+=vx[jx][jy][jz]*bx[kx][ky][kz]; vvxy[ix][iy][iz]+=vx[jx][jy][jz]*by[kx][ky][kz]; vvxz[ix][iy][iz]+=vx[jx][jy][jz]*bz[kx][ky][kz];
-							vvyx[ix][iy][iz]+=vy[jx][jy][jz]*bx[kx][ky][kz]; vvyy[ix][iy][iz]+=vy[jx][jy][jz]*by[kx][ky][kz]; vvyz[ix][iy][iz]+=vy[jx][jy][jz]*bz[kx][ky][kz];
-							vvzx[ix][iy][iz]+=vz[jx][jy][jz]*bx[kx][ky][kz]; vvzy[ix][iy][iz]+=vy[jx][jy][jz]*by[kx][ky][kz]; vvzz[ix][iy][iz]+=vz[jx][jy][jz]*bz[kx][ky][kz];
+							vbxx[ix][iy][iz]+=vx[jx][jy][jz]*bx[kx][ky][kz]*DV[jx][jy][jz]; vbxy[ix][iy][iz]+=vx[jx][jy][jz]*by[kx][ky][kz]*DV[jx][jy][jz]; vbxz[ix][iy][iz]+=vx[jx][jy][jz]*bz[kx][ky][kz]*DV[jx][jy][jz];
+							vbyx[ix][iy][iz]+=vy[jx][jy][jz]*bx[kx][ky][kz]*DV[jx][jy][jz]; vbyy[ix][iy][iz]+=vy[jx][jy][jz]*by[kx][ky][kz]*DV[jx][jy][jz]; vbyz[ix][iy][iz]+=vy[jx][jy][jz]*bz[kx][ky][kz]*DV[jx][jy][jz];
+							vbzx[ix][iy][iz]+=vz[jx][jy][jz]*bx[kx][ky][kz]*DV[jx][jy][jz]; vbzy[ix][iy][iz]+=vz[jx][jy][jz]*by[kx][ky][kz]*DV[jx][jy][jz]; vbzz[ix][iy][iz]+=vz[jx][jy][jz]*bz[kx][ky][kz]*DV[jx][jy][jz];
 						}
 					}
 				}
@@ -77,29 +83,29 @@ int main(void)
 	}
 
 	for(int ix=0;ix<=Lx; ix++)
-	{int Qx=-Qx0+ix*DQx;
+	{double Qx=-Qx0+ix*DQx;
 		for(int iy=0;iy<=Ly; iy++)
-		{int Qy=-Qy0+iy*DQy;
+		{double Qy=-Qy0+iy*DQy;
 			for(int iz=0;iz<=Lz; iz++)
-			{int Qz=-Qz0+iz*DQz;
+			{double Qz=-Qz0+iz*DQz;
 			// velociped
-		   rnvx[ix][iy][iz]=(vvxx[ix][iy][iz]+bbxx[ix][iy][iz])*Qx+(vvxy[ix][iy][iz]+bbxy[ix][iy][iz])*Qy+(vvxz[ix][iy][iz]+bbxz[ix][iy][iz])*Qz;
-			 rnvy[ix][iy][iz]=(vvyx[ix][iy][iz]+bbyx[ix][iy][iz])*Qx+(vvyy[ix][iy][iz]+bbyy[ix][iy][iz])*Qy+(vvyz[ix][iy][iz]+bbyz[ix][iy][iz])*Qz;
-       rnvz[ix][iy][iz]=(vvzx[ix][iy][iz]+bbzx[ix][iy][iz])*Qx+(vvzy[ix][iy][iz]+bbzy[ix][iy][iz])*Qy+(vvzz[ix][iy][iz]+bbzz[ix][iy][iz])*Qz;
+				rnvx[ix][iy][iz]=(vvxx[ix][iy][iz]+bbxx[ix][iy][iz])*Qx+(vvxy[ix][iy][iz]+bbxy[ix][iy][iz])*Qy+(vvxz[ix][iy][iz]+bbxz[ix][iy][iz])*Qz;
+				rnvy[ix][iy][iz]=(vvyx[ix][iy][iz]+bbyx[ix][iy][iz])*Qx+(vvyy[ix][iy][iz]+bbyy[ix][iy][iz])*Qy+(vvyz[ix][iy][iz]+bbyz[ix][iy][iz])*Qz;
+				rnvz[ix][iy][iz]=(vvzx[ix][iy][iz]+bbzx[ix][iy][iz])*Qx+(vvzy[ix][iy][iz]+bbzy[ix][iy][iz])*Qy+(vvzz[ix][iy][iz]+bbzz[ix][iy][iz])*Qz;
 			// magnitno pole
-			 rnbx[ix][iy][iz]=(bvxx[ix][iy][iz]-vbxx[ix][iy][iz])*Qx+(bvxy[ix][iy][iz]-vbxy[ix][iy][iz])*Qy+(bvxz[ix][iy][iz]-vbxz[ix][iy][iz])*Qz;
-			 rnby[ix][iy][iz]=(bvyx[ix][iy][iz]-vbyx[ix][iy][iz])*Qx+(bvyy[ix][iy][iz]-vbyy[ix][iy][iz])*Qy+(bvyz[ix][iy][iz]-vbyz[ix][iy][iz])*Qz;
-       rnbz[ix][iy][iz]=(bvzx[ix][iy][iz]-vbzx[ix][iy][iz])*Qx+(bvzy[ix][iy][iz]-vbzy[ix][iy][iz])*Qy+(bvzz[ix][iy][iz]-vbzz[ix][iy][iz])*Qz;
+				rnbx[ix][iy][iz]=(bvxx[ix][iy][iz]-vbxx[ix][iy][iz])*Qx+(bvxy[ix][iy][iz]-vbxy[ix][iy][iz])*Qy+(bvxz[ix][iy][iz]-vbxz[ix][iy][iz])*Qz;
+				rnby[ix][iy][iz]=(bvyx[ix][iy][iz]-vbyx[ix][iy][iz])*Qx+(bvyy[ix][iy][iz]-vbyy[ix][iy][iz])*Qy+(bvyz[ix][iy][iz]-vbyz[ix][iy][iz])*Qz;
+				rnbz[ix][iy][iz]=(bvzx[ix][iy][iz]-vbzx[ix][iy][iz])*Qx+(bvzy[ix][iy][iz]-vbzy[ix][iy][iz])*Qy+(bvzz[ix][iy][iz]-vbzz[ix][iy][iz])*Qz;
 			}
 		}
 	}
 
 	for(int ix=0;ix<=Lx; ix++)
-	{int Qx=-Qx0+ix*DQx;
+	{double Qx=-Qx0+ix*DQx;
 		for(int iy=0;iy<=Ly; iy++)
-		{int Qy=-Qy0+iy*DQy;
+		{double Qy=-Qy0+iy*DQy;
 			for(int iz=0;iz<=Lz; iz++)
-			{int Qz=-Qz0+iz*DQz;
+			{double Qz=-Qz0+iz*DQz;
 		     Q=sqrt(Qx*Qx+Qy*Qy+Qz*Qz); if (Q==0.0) continue;
 			 rnx=Qx/Q; rny= Qy/Q; rnz= Qz/Q;
 			 vn=rnvx[ix][iy][iz]*rnx+ rnvy[ix][iy][iz]*rny+rnvz[ix][iy][iz]*rnz;
@@ -107,10 +113,10 @@ int main(void)
 			 // velociped projection
 			 rnvx[ix][iy][iz]-=rnx*vn; rnvy[ix][iy][iz]-=rny*vn; rnvz[ix][iy][iz]-=rnz*vn;
 			 // magnetic projection
-			 rnbx[ix][iy][iz]-=rnx*bn; rnby[ix][iy][iz]-=rny*bn; rnbz[ix][iy][iz]-=rnz*bn; // <<<<<<<<<<< DA SE PROVERI FORMULATA
+			 rnbx[ix][iy][iz]-=rnx*bn; rnby[ix][iy][iz]-=rny*bn; rnbz[ix][iy][iz]-=rnz*bn; 
 			}
 		}
 	}
-
+vvxx[ix][iy][iz]=?=exp(-.5*(Qx*Qx+Qy*Qy+Qz*Qz))/(sqrt(8.*M_PI)*sqrt(8.*M_PI)*sqrt(8.*M_PI));
 	return 0;
 }
