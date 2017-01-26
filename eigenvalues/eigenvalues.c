36198988e85f689eb10e301d87b1c827b4233422
Zlatan Dimitrov <zlatan.dimitrov@gmail.com>
2017-01-26 14:00:54 +0200

    Fix 6D

--- eigenvalues/eigenvalues.c
+++ eigenvalues/eigenvalues.c
@@ -17,43 +17,56 @@ int main (void)
   {
   double data[] = { 0.0, 0.0, 0.0, -Qa, 0.0, 0.0,
                     1.0, 0.0, 0.0, 0.0, -Qa, 0.0,
-                    0.0, 0.0, 0.0, 0.0, 0.0, -Qa, 
+                    0.0, 0.0, 0.0, 0.0, 0.0, -Qa,
                     Qa , 0.0, 0.0, 2*ny*nx*(w+1)        , -2*nx*nx*w+2*w , 0,
-		    0.0, Qa , 0.0, 2*ny*ny*(w+1)-(2*w+1), -2*nx*ny*w     , 0,
-		    0.0, 0.0, Qa , 2*ny*nz*(w+1)        , -2*nx*nz*w	 , 0 };
+		                0.0, Qa , 0.0, 2*ny*ny*(w+1)-(2*w+1), -2*nx*ny*w     , 0,
+		                0.0, 0.0, Qa , 2*ny*nz*(w+1)        , -2*nx*nz*w	   , 0 };
 
   gsl_matrix_view m = gsl_matrix_view_array (data, 6, 6);
-  gsl_vector *eval = gsl_vector_alloc (6);
-  gsl_matrix *evec = gsl_matrix_alloc (6, 6);
-  gsl_eigen_symmv_workspace * ws = gsl_eigen_symmv_alloc (6);
-  gsl_eigen_symmv (&m.matrix, eval, evec, ws);
-  gsl_eigen_symmv_free (ws);
-  gsl_eigen_symmv_sort (eval, evec, GSL_EIGEN_SORT_ABS_ASC);
-  
+  gsl_vector_complex *eval = gsl_vector_complex_alloc (6);
+  gsl_matrix_complex *evec = gsl_matrix_complex_alloc (6, 6);
+
+  gsl_eigen_nonsymmv_workspace * w = gsl_eigen_nonsymmv_alloc (6);
+  gsl_eigen_nonsymmv (&m.matrix, eval, evec, w);
+  gsl_eigen_nonsymmv_free (w);
+  gsl_eigen_nonsymmv_sort (eval, evec, GSL_EIGEN_SORT_ABS_DESC);
+
   {
     int i;
     double tmp[7];
+    double vx[7],vy[7];
     for (i = 0; i < 6; i++)
-      { 
-        double eval_i = gsl_vector_get (eval, i);
-        gsl_vector_view evec_i = gsl_matrix_column (evec, i);
-	tmp[i] = eval_i*eval_i;
-        //printf ("%g %g\n",Qa,eval_i);
+      {
+        gsl_complex eval_i = gsl_vector_complex_get (eval, i);
+ 	      gsl_complex labdasq = gsl_complex_mul(eval_i,eval_i);
+	      tmp[i] = GSL_REAL(labdasq);
+        gsl_vector_complex_view evec_i = gsl_matrix_complex_column (evec, i);
+
+        vx[i]=GSL_REAL(gsl_vector_complex_get(&evec_i.vector, 2));
+        vy[i]=GSL_REAL(gsl_vector_complex_get(&evec_i.vector, 3));
+
+        //printf ("value -> %g\n", eval_i*eval_i);
 
         //printf ("eigenvector = \n");
         //gsl_vector_fprintf (stdout, &evec_i.vector, "%g");
       }
     tmp[7]=tmp[0];
     for (i = 0; i < 6; i++)
-      { 
+      {
        if ( tmp[i] > tmp[7] )
+	{
 	tmp[7] = tmp[i];
+	vx[7]  = vx[i];
+  vy[7]  = vy[i];
+        }
       }
-     printf ("%g %g\n",Qa,tmp[7]);
+     printf ("%g %g %g\n",Qa,vx[7],vy[7]);
   }
+
 
   gsl_vector_free (eval);
   gsl_matrix_free (evec);
   }
+
   return 0;
 }
